b3bffc3d393c7128c7a50e1a0ebb8e67ec3cf1a3
Mark Hansen <mark@markhansen.co.nz>
2011-06-05 01:29:55 +1200

    add borders to the table

--- site.rb
+++ site.rb
@@ -7,6 +7,8 @@ get '/' do
   date = if params.has_key? 'date' then params['date'] else Date.today.to_s end
   [0, 1].map { |i|
     doc = Hpricot(open("http://www.flightstats.com/go/FlightStatus/flightStatusByAirport.do?airport=%28HLZ%29+Hamilton+Airport%2C+NZ&airportQueryType=#{i}&airportQueryDate=" + date))
-    doc.search(".tableListingTable").to_s
+    table = doc.search(".tableListingTable")[0]
+    table.raw_attributes = { "border" => "1" }
+    table.to_s
   }.join("<p>")
 end
